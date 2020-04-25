7b51189f3d0583b127e1d1d7b80418bcc56d46b6
VandanRogheliya <63041915+VandanRogheliya@users.noreply.github.com>
2020-04-25 22:39:46 +0530

    Update settings.py

--- NP/settings.py
+++ NP/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '192.168.43.161',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
